e12b179141f0810bc892724d2ebf821d3a3f8a8c
vijay-kumar-singh <vijaysingh.nl@gmail.com>
2020-04-12 15:57:59 +0200

    Updates

---
+++ precache-manifest.7f6ca9df6b98f593c6d42af29e6e414b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3963b656ca7bb343aa2e1de3c0b50e35",
+    "url": "/react-portal-app/index.html"
+  },
+  {
+    "revision": "6696d04fd4637cabb186",
+    "url": "/react-portal-app/static/css/main.424ad101.chunk.css"
+  },
+  {
+    "revision": "ca3105ac2a0550314ff9",
+    "url": "/react-portal-app/static/js/2.4e294910.chunk.js"
+  },
+  {
+    "revision": "10d6f33f4ce4f07cb83f",
+    "url": "/react-portal-app/static/js/3.58d890cc.chunk.js"
+  },
+  {
+    "revision": "4951dbd9ecb02550de0c",
+    "url": "/react-portal-app/static/js/4.635ec26c.chunk.js"
+  },
+  {
+    "revision": "c4f1440a60e23197cbf6",
+    "url": "/react-portal-app/static/js/5.3eb8b2ba.chunk.js"
+  },
+  {
+    "revision": "4bd6cc4bb55fdbb71801",
+    "url": "/react-portal-app/static/js/6.dd812a6d.chunk.js"
+  },
+  {
+    "revision": "29dd642d47958064ec57",
+    "url": "/react-portal-app/static/js/7.d3a9041c.chunk.js"
+  },
+  {
+    "revision": "6696d04fd4637cabb186",
+    "url": "/react-portal-app/static/js/main.de58d577.chunk.js"
+  },
+  {
+    "revision": "c23080c6f4a2eed83b53",
+    "url": "/react-portal-app/static/js/runtime~main.452b2d11.js"
+  },
+  {
+    "revision": "537bb5f460534d156ef2d7319d285225",
+    "url": "/react-portal-app/static/media/defaultBcg.537bb5f4.jpg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/react-portal-app/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "cf6a5a10d2c4daa2dccc79a1274e21d1",
+    "url": "/react-portal-app/static/media/product-1.cf6a5a10.jpeg"
+  },
+  {
+    "revision": "26efff627092ed81a4a6116b9cb93d4c",
+    "url": "/react-portal-app/static/media/product-10.26efff62.jpeg"
+  },
+  {
+    "revision": "1ded4bd6676d0b480ad1a87ba2a2f12f",
+    "url": "/react-portal-app/static/media/product-2.1ded4bd6.gif"
+  },
+  {
+    "revision": "881d7c3cbc6dc26753097bc684c6c2d1",
+    "url": "/react-portal-app/static/media/product-5.881d7c3c.jpeg"
+  },
+  {
+    "revision": "ce84f7ae8dc98dc3acdbb46825b44834",
+    "url": "/react-portal-app/static/media/product-6.ce84f7ae.jpeg"
+  },
+  {
+    "revision": "8d9720e34e4b32ff22422fae0b3d85c4",
+    "url": "/react-portal-app/static/media/product-9.8d9720e3.jpeg"
+  }
+]);
