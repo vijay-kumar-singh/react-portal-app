ba5948764d93009026eb2c8beee968a1e68181ca
vijay-kumar-singh <vijaysingh.nl@gmail.com>
2020-04-13 14:33:17 +0200

    added material-ui component library

--- src/components/CovidStats.tsx
+++ src/components/CovidStats.tsx
@@ -4,54 +4,67 @@ import useStats from '../services/useCovidStats';
 import ErrorPage from '../pages/ErrorPage';
 
 const StatGrid = styled.div`
-  padding-top: 100px;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 1rem;
-  @media screen and (max-width: 576px) {
-    display: block;
-    margin-top: 100px;
-  }
+    padding-top: 100px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 1rem;
+    @media screen and (max-width: 576px) {
+        display: block;
+        margin-top: 100px;
+    }
 `;
-const StatBlock = styled.div`
-  background: #f2f2f2;
-  font-size: 2rem;
-  padding: 2rem;
-  border-radius: 2rem;
-  display: grid;
-  align-items: center;
-  justify-items: center;
-  text-align: center;
-  border: 3px solid ${(props) => props.color};
+const StatBlock = styled(Card)`
+    background: ${(props) => props.color};
+    font-size: 2rem;
+    padding: 2rem;
+    border-radius: 2rem;
+    display: grid;
+    align-items: center;
+    justify-items: center;
+    text-align: center;
+    border: 3px solid ${(props) => props.color};
 `;
-
+const Wrapper = styled(Card)`
+    padding: 4em;
+    margin: auto;
+    width: 100%;
+    background: #af9a7d;
+`;
+const Title = styled.h1`
+    text-align: center;
+    font-size: 1.5em;
+    color: white;
+`;
 interface CovidProps {
     url: string;
 }
 
 const Stats: React.FC<CovidProps> = ({ url }) => {
-  const { stats, loading, error } = useStats(url);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <ErrorPage/>;
- 
-  return (
-    <>
-    <StatGrid>
-      <StatBlock color="blue">
-        <h3>Confirmed:</h3>
-        <span>{stats.confirmed.value}</span>
-      </StatBlock>
-      <StatBlock color="red">
-        <h3>Deaths:</h3>
-        <span>{stats.deaths.value}</span>
-      </StatBlock>
-      <StatBlock color="green">
-        <h3>Recovered:</h3>
-        <span>{stats.recovered.value}</span>
-      </StatBlock>
-    </StatGrid>
-    </>
-  );
-}
+    const { stats, loading, error } = useStats(url);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <ErrorPage />;
+
+    return (
+        <>
+            <Wrapper>
+                <Title>World wide COVID-19 corona virus pandemic status</Title>
+                <StatGrid>
+                    <StatBlock color="blue">
+                        <Title>Confirmed:</Title>
+                        <Title>{stats.confirmed.value}</Title>
+                    </StatBlock>
+                    <StatBlock color="red">
+                        <Title>Deaths:</Title>
+                        <Title>{stats.deaths.value}</Title>
+                    </StatBlock>
+                    <StatBlock color="green">
+                        <Title>Recovered:</Title>
+                        <Title>{stats.recovered.value}</Title>
+                    </StatBlock>
+                </StatGrid>
+            </Wrapper>
+        </>
+    );
+};
 
 export default Stats;
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Loading from '../components/Loading';
 import GlobalStyles from '../globalStyles';
+import { StylesProvider } from '@material-ui/styles';
 import styled, { ThemeProvider } from 'styled-components';
 import { colors } from '../utils/color';
 
@@ -14,9 +15,6 @@ const ProductDetails = lazy(() => import('../pages/ProductDetails'));
 const Covid19Stats = lazy(() => import('../pages/Covid19Stats'));
 const ErrorPage = lazy(() => import('../pages/ErrorPage'));
 
-
-
-
 const AppContainer = styled.div`
     background: ${(props) => props.theme.siteBackground};
     min-height: 100vh;
@@ -25,23 +23,25 @@ const AppContainer = styled.div`
 
 const App: React.FC = () => {
     return (
-        <ThemeProvider theme={colors.light}>
-            <AppContainer>
-                <React.Fragment>
-                   <GlobalStyles />
-                    <Navbar />
-                    <Suspense fallback={<Loading/>}>
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/products/" component={Products} />
-                        <Route exact path="/products/:slug" component={ProductDetails} />
-                        <Route exact path="/Covid-19Stats" component={Covid19Stats} />
-                        <Route component={ErrorPage} />
-                    </Switch>
-                    </Suspense>
-                </React.Fragment>
-            </AppContainer>
-        </ThemeProvider>
+        <StylesProvider injectFirst>
+            <ThemeProvider theme={colors.light}>
+                <AppContainer>
+                    <React.Fragment>
+                        <GlobalStyles />
+                        <Navbar />
+                        <Suspense fallback={<Loading />}>
+                            <Switch>
+                                <Route exact path="/" component={Home} />
+                                <Route exact path="/products/" component={Products} />
+                                <Route exact path="/products/:slug" component={ProductDetails} />
+                                <Route exact path="/Covid-19Stats" component={Covid19Stats} />
+                                <Route component={ErrorPage} />
+                            </Switch>
+                        </Suspense>
+                    </React.Fragment>
+                </AppContainer>
+            </ThemeProvider>
+        </StylesProvider>
     );
 };
 
